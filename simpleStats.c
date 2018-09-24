1510e91b89cf06563b78a84403c4b65c0d0a2519
David Liotta <liottad6@students.rowan.edu>
2018-09-24 12:51:54 -0400

    Changed but still not working yet

--- simpleStats.c
+++ simpleStats.c
@@ -6,42 +6,37 @@
  */
 
 #include <stdio.h>
-#include <string.h>
+
+#define BUFSIZE 256
 
 int main(){
 	/**
 	 * Setting up some variables to utilize in this program
 	 */
 	double n, max, min, sum, mean;
-
-	char uI[64];
-
-	printf("a");		//Test line
-	fgets(uI, 63, stdin);
-	scanf("%lf", &n);	//Scanning in the first number for n
-
-	printf("b");		//test
+	char line[BUFSIZE];
+	int numsRead = 0;
+	int numOfItems = 1;  //This is because we scanned for n earlier
+	n = -1;
 	max = n;
 	min = n;
 	sum = n;
-	int numOfItems = 1;  //This is because we scanned for n earlier
 
-	while(n != EOF && n != 0){
-		printf("c");		//test
-		if(n > max)
-			max = n;
-	    if(n < min)
-	        min = n;
-	    sum = sum + n;
-	    numOfItems++;
-	    sscanf(uI, "%f.3", &n);
+	while(n != 0 && fgets(line, BUFSIZE, stdin ) != NULL){
+		numsRead = sscanf(line, "%f", &n);
+		if(numsRead == 1 && n != 0){
+			numOfItems++;
+			if(n > max)
+				max = n;
+			if(n < min)
+				min = n;
+			sum = sum + n;
+		}
+		if(numsRead == 0)
+			printf("Bad input\n");
 	}
 
-	if(n == 0 || n == EOF)
-		printf("Bad input");
-
 	mean = sum / numOfItems;
-
 
 	printf("# of items: %i", numOfItems);
 	printf("\nSum: %f.3", sum);
