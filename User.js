8334443f1f74dd7e7f660bc7bf272eb72dff59b4
David Liotta <liottad6@students.rowan.edu>
2018-10-10 14:27:40 -0400

    Update User.js

--- User.js
+++ User.js
@@ -15,18 +15,13 @@ function main(){
     *   The have many things stored about them
     */
     class User {
-        constructor(user, pass) {
-            if(userList.includes(user)){
-                console.log("This user already exists!")
-            }else{
-                this.userName = user;
-                this.password = pass; //TODO want to store this as a hashed value for security
-                var email;
-                var surveyList = [];
-                var isAdmin = false;
-                var userPic;
-                var sList = 0;
-            }
+        constructor(email, user, pass) {
+            this.userName = user;
+            this.password = pass; //TODO want to store this as a hashed value for security
+            this.email = email;
+            var surveyList = [];
+            var isAdmin = false;
+            var userPic;
         }
         /*
          *   Allows Users to set their email
@@ -57,6 +52,9 @@ function main(){
             this.password = newPass.hashCode();
         }
 
+        /**
+         * Used to filter a user out of our userList
+         */
         deleteUser(userName){
             if(this.isAdmin == true || this.userName == userName)
                 userList = userList.filter(item => item !== userName)
@@ -85,17 +83,33 @@ function main(){
         return hash;
     }
 
+    //TODO email duplication checking does not work yet
     //This and createAdmin could probably be one function
-    function createUser(userName, password){
-        var userName = new User(userName, password);
-        userList.push(userName);
+    function createUser(email, userName, password, repass){
+        if(!userList.includes(email)){
+            if(password == repass){
+                var userName = new User(email, userName, password);
+                userList.push(userName);
+            }else{
+                console.log("The passwords don't match!")
+            }
+        }else{
+            console.log("This user already exists");
+        } 
     }
 
-    function createAdmin(admin, password){
-        var temp = admin;
-        temp = new User(admin, password);
-        temp.isAdmin = true;
-        userList.push(temp);
+    function createAdmin(email, adminName, password, repass){
+        if(!userList.includes(email)){
+            if(password == repass){
+                var admin = new User(email, adminName, password);
+                admin.isAdmin = true;
+                userList.push(admin);
+            }else{
+                console.log("The passwords don't match!")
+            }
+        }else{
+            console.log("This user already exists");
+        } 
     }
 
     /*
@@ -106,20 +120,20 @@ function main(){
             console.log(userList.indexOf(i).userName)
     }
     
-    createAdmin("user1", "pass123");
-    createUser("user2", "pass456");
-    createUser("user3", "pass789");
-    createUser("user4", "pass012");
-    createUser("user3", "pass789");
+    createAdmin("a1@users.com", "admin", "pass123", "pass123");
+    createUser("u1@users.com", "user1", "pass456", "pass456");
+    createUser("u2@users.com", "user2", "pass789", "pass789");
+    createUser("u3@users.com", "user3", "pass012", "pass012");
+    createUser("u3@users.com", "user3", "pass789", "pass789");  //should not be able to create this user for due to repeated email
+    createUser("u4@users.com", "user4", "pass000", "wrong");  //should not be able to create this user for due passwords not matching
     
     console.log(userList)
-    var admin = new User("radar", "stuff");
+    var admin = new User("fake@email.com", "radar", "stuff");
     admin.isAdmin = true;
 
-    admin.deleteUser(User.userName)
-
-    console.log(uName)
+    admin.deleteUser(userList.user2)
 
     console.log(userList)
 
 }
+
